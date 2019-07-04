72cbe95fce71e08771afa74457855675fc0e6c4e
Pham Tuan <phamtuan199911@gmail.com>
2019-07-04 17:55:20 +0700

    Fix read map Add manhattan heuristic Add skeleton to paint

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
+import time
+from random import randrange
+
 import wx
 import numpy
 
 
-def readMap(mapname):
-    map_file = open(mapname, "r", encoding='utf8')
-    # meow viet doc file o day :3
-    # Read 2 number as size of map N x M.
+def read_map(map_name):
+    map = []
+    map_file = open(map_name, "r", encoding='utf8')
     lines = map_file.readlines()
     map_file.close()
-    map_matrix = list()
-    if lines:
-        map_height, map_width = map(int(lines[0].split()))
-        map_matrix.extend(list(lines))
-        map_matrix.remove(map_matrix[0])
-        map_matrix.remove(map_matrix[len(map_matrix) - 1])
-    return map_matrix
+    del lines[0]
+    x_pacman_pos, y_pacman_pos = [int(x)
+                                  for x in lines[len(lines) - 1].split(' ')]
+    del lines[-1]
+    for line in lines:
+        map.append(list(line))
+    return map, (x_pacman_pos, y_pacman_pos)
 
 
 class AStarAgent:
-    def __init__(self, map):
-        self.map = map
+    def __init__(self, maze_map, start_pos):
+        self.map = maze_map
         self.path = self.__a_star(self.map)
+        self.start_pos = start_pos
         self.stepCount = -1
 
     def get_next_step(self):
@@ -28,8 +31,17 @@ class AStarAgent:
         return self.path[self.stepCount]
 
     @staticmethod
+    # a* search alg
     def __a_star(map):
-        return []
+        return []  # list of position
+
+    @staticmethod
+    def __manhattan_heuristic(from_pos, to_pos):
+        return abs(from_pos[0] - to_pos[0])\
+               + abs(from_pos[1] - to_pos[1])
+
+    def is_finished(self):
+        return self.stepCount == len(self.path) - 1
 
 
 class Node:
@@ -47,22 +59,26 @@ class GameFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
         super(GameFrame, self).__init__(*args, **kwargs)
         self.SetSize(800, 600)
-        self.panel = DrawPanel(self)
-        self.panel.Bind(wx.EVT_PAINT, self.onPaint)
+        self.agent = None
+        self.current_position = None
 
-    def onPaint(self, event):
-        drawer = wx.PaintDC(self.panel)
-        drawer.DrawLine(0, 10, 10, 10)
+    def paint(self):
+        dc = wx.ClientDC(self)
+        dc.Clear()
+        # draw map here
+        #
 
-
-class DrawPanel(wx.Panel):
-    def __init__(self, *args, **kwargs):
-        super(DrawPanel, self).__init__(*args, **kwargs)
+    def start(self):
+        while not self.agent.is_finished():
+            self.current_position = self.agent.get_next_step()
+            self.paint()
 
 
 if __name__ == '__main__':
     app = wx.App()
+    maze_map, start_position = read_map(".\\test\\maps\\demo01.txt")
     game_frame = GameFrame(None, title="Test")
+    game_frame.current_position = start_position
+    game_frame.agent = AStarAgent(maze_map, start_position)
     game_frame.Show()
-
     app.MainLoop()
