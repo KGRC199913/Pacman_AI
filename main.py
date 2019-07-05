336dd31cdb800529c529baaab4bcfb298d30f778
Pham Tuan <phamtuan199911@gmail.com>
2019-07-05 14:42:53 +0700

    Add A* Fix bugs

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ cellSize = 5
 
 
 def read_map(map_name):
-    map = []
+    maze_map = []
     map_file = open(map_name, "r", encoding='utf8')
     lines = map_file.readlines()
     map_file.close()
@@ -22,8 +22,9 @@ def read_map(map_name):
                                   for x in lines[len(lines) - 1].split(' ')]
     del lines[-1]
     for line in lines:
-        map.append(list(line))
-    return map, (x_pacman_pos, y_pacman_pos)
+        maze_map.append([int(x) for x in list(line.rstrip('\n'))])
+
+    return maze_map, (x_pacman_pos, y_pacman_pos)
 
 
 class AStarAgent:
@@ -31,7 +32,7 @@ class AStarAgent:
         self.map = maze_map
 
         start_node = Node(position=start_pos)
-        end_node = AStarAgent.__find_food(maze_map)
+        end_node = Node(position=AStarAgent.__find_food(maze_map))
 
         self.path = self.__a_star(self.map, start_node, end_node)
         self.start_pos = start_pos
@@ -43,8 +44,8 @@ class AStarAgent:
 
     @staticmethod
     def __manhattan_heuristic(from_pos, to_pos):
-        return abs(from_pos[0] - to_pos[0]) \
-               + abs(from_pos[1] - to_pos[1])
+        return abs(from_pos.position[0] - to_pos.position[0]) \
+               + abs(from_pos.position[1] - to_pos.position[1])
 
     # find least f node for A*
     @staticmethod
@@ -59,40 +60,49 @@ class AStarAgent:
     # find least f node for A*
     def __generate_childs(maze_map, current_node):
         childs = []
+        x = current_node.position[0]
+        y = current_node.position[1]
 
-        if maze_map[current_node.position[0] + 1][current_node.position[1] + 1] == 0:
-            childs.pop(current_node.position[0] + 1)
-            childs.pop(current_node.position[1] + 1)
+        child = maze_map[x][y + 1]
+        if (child == 0) or (child == 2):
+            childs.append(Node(parent=current_node,
+                               position=(x, y + 1)))
 
-        if maze_map[current_node.position[0] + 1][current_node.position[1] - 1] == 0:
-            childs.pop(current_node.position[0] + 1)
-            childs.pop(current_node.position[1] - 1)
+        child = maze_map[x][y - 1]
+        if (child == 0) or (child == 2):
+            childs.append(Node(parent=current_node,
+                               position=(x, y - 1)))
 
-        if maze_map[current_node.position[0] - 1][current_node.position[1] + 1] == 0:
-            childs.pop(current_node.position[0] - 1)
-            childs.pop(current_node.position[1] + 1)
+        child = maze_map[x + 1][y]
+        if (child == 0) or (child == 2):
+            childs.append(Node(parent=current_node,
+                               position=(x + 1, y)))
 
-        if maze_map[current_node.position[0] - 1][current_node.position[1] - 1] == 0:
-            childs.pop(current_node.position[0] - 1)
-            childs.pop(current_node.position[1] - 1)
-
-        for child in childs:
-            child.parent = current_node
+        child = maze_map[x - 1][y]
+        if (child == 0) or (child == 2):
+            childs.append(Node(parent=current_node,
+                               position=(x - 1, y)))
 
         return childs
 
     @staticmethod
     def __find_food(maze_map):
-        for pos in maze_map:
-            if pos == 2:
-                return Node(position=pos)
+        if maze_map is None:
+            return None
+        height = len(maze_map)
+        width = len(maze_map[0])
+        for i in range(height):
+            for j in range(width):
+                if maze_map[i][j] == 2:
+                    return i, j
+        return None
 
     @staticmethod
     # a* search alg
     def __a_star(maze_map, start_node, end_node):
         open_list = []
         closed_list = []
-        result_list = []
+        path = []
         if start_node is not end_node:
             open_list.append(start_node)
 
@@ -101,11 +111,10 @@ class AStarAgent:
             closed_list.append(open_list.pop(open_list.index(current_node)))
 
             if current_node == end_node:
-                while current_node is not start_node:
-                    result_list.append(current_node)
+                while current_node is not None:
+                    path.append(current_node)
                     current_node = current_node.parent
-                result_list.append(start_node)
-                return result_list
+                return path[::-1]
 
             childs = AStarAgent.__generate_childs(maze_map, current_node)
 
@@ -113,15 +122,13 @@ class AStarAgent:
                 if child in closed_list:
                     continue
                 child.g = current_node.g + 1
-                child.h = AStarAgent.__manhattan_heuristic(child,end_node)
+                child.h = AStarAgent.__manhattan_heuristic(child, end_node)
                 child.f = child.g + child.h
 
                 if child in open_list:
                     if child.g > current_node.g:
                         continue
                 open_list.append(child)
-
-        return result_list
 
     def is_finished(self):
         return self.stepCount == len(self.path) - 1
@@ -178,9 +185,10 @@ class GameFrame(wx.Frame):
 
 if __name__ == '__main__':
     app = wx.App()
-    maze_map, start_position = read_map(".\\test\\maps\\demo01.txt")
+    maze_map, start_position = read_map(".\\test\\maps\\demo02.txt")
     game_frame = GameFrame(None, title="Test")
     game_frame.current_position = start_position
     game_frame.agent = AStarAgent(maze_map, start_position)
     game_frame.Show()
+    game_frame.start()
     app.MainLoop()
